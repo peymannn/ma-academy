5ea6a0e272f0fce2f9b3f3f1d7f2b2be68d773f9
Peyman Öter <peymanoter@gmail.com>
2019-10-28 22:04:15 +0300

    update3

--- src/main/java/repetitiveChars.java
+++ src/main/java/repetitiveChars.java
@@ -1,24 +1,26 @@
+
 public class repetitiveChars {
     public static void main(String[] args) {
-        String characters="APLPbCD";
-        ArrayCharracter(characters);
+        String characters="ALKCCCD";
+        System.out.println(FindRepetitiveCharacter(characters));
     }
 
-    private static void ArrayCharracter(String characters) {
-        String newCharacters="";
-        for (int i = 0; i <characters.length() ; i++) {
-            newCharacters= characters.replaceAll(characters.charAt(i)+"","" );
+    private static char FindRepetitiveCharacter(String characters) {
+        String newCharacters = "";
+        char resultChar=' ';
+        for (int i = 0; i < characters.length(); i++) {
+            newCharacters = characters.replaceAll(characters.charAt(i) + "", "");
 
-          if( newCharacters.length()<characters.length()-1)
-          {
-              System.out.println(characters.charAt(i));
-              break;
-          }else{
-              if(i!=characters.length()-1)
-                  continue;
-              else
-                  System.out.println(" ");
-          }
+            if (newCharacters.length() < characters.length() - 1) {
+                resultChar= characters.charAt(i);
+            } else {
+                if (i != characters.length() - 1)
+                    continue;
+                else
+                    break;
+            }
         }
+        return resultChar;
     }
 }
+
