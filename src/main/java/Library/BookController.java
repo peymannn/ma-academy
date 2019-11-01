63f633f3cbc6119778d621320ce13e23afe261e5
Peyman Öter <peymanoter@gmail.com>
2019-11-01 23:15:29 +0300

    Update BookController.java

--- src/main/java/Library/BookController.java
+++ src/main/java/Library/BookController.java
@@ -1,127 +1,42 @@
 package Library;
 
-import java.sql.SQLOutput;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+
+import java.util.*;
 
 public class BookController {
-    List<Book> bookList=new List<Book>() {
-        public int size() {
-            return bookList.size();
-        }
+    static ArrayList<Book> bookList=new ArrayList<Book>();
+    public static void main(String[] args) {
+        Book book=new Book(1,"Peyman Otobiyografi","Peyman",2019,"Avare",15.5);
+        create(book);
+        getAllBookList();
+        delete(1);
+        getAllBookList();
+    }
 
-        public boolean isEmpty() {
-            return false;
-        }
-        public boolean contains(Object o) {
-            return false;
-        }
-        public Iterator<Book> iterator() {
-            return null;
-        }
-
-        public Object[] toArray() {
-            return new Object[0];
-        }
-
-        public <T> T[] toArray(T[] a) {
-            return null;
-        }
-
-        public boolean add(Book book) {
-            return false;
-        }
-
-        public boolean remove(Object o) {
-            return false;
-        }
-
-        public boolean containsAll(Collection<?> c) {
-            return false;
-        }
-
-        public boolean addAll(Collection<? extends Book> c) {
-            return false;
-        }
-
-        public boolean addAll(int index, Collection<? extends Book> c) {
-            return false;
-        }
-
-        public boolean removeAll(Collection<?> c) {
-            return false;
-        }
-
-        public boolean retainAll(Collection<?> c) {
-            return false;
-        }
-
-        public void clear() {
-
-        }
-
-        public Book get(int index) {
-            return null;
-        }
-
-        public Book set(int index, Book element) {
-            return null;
-        }
-
-        public void add(int index, Book element) {
-
-        }
-
-        public Book remove(int index) {
-            return null;
-        }
-
-        public int indexOf(Object o) {
-            return 0;
-        }
-
-        public int lastIndexOf(Object o) {
-            return 0;
-        }
-
-        public ListIterator<Book> listIterator() {
-            return null;
-        }
-
-        public ListIterator<Book> listIterator(int index) {
-            return null;
-        }
-
-        public List<Book> subList(int fromIndex, int toIndex) {
-            return null;
-        }
-    };
-    Book book=new Book(1,"Peyman Otobiyografi","Peyman",2019,"Avare",15.5);
-    private void create(Book book){
+        public static void create(Book book){
         bookList.add(book);
     }
-    private void delete(int bookID){
+        public  static void delete(int bookID){
         bookList.remove(getByBookId(bookID));
     }
-    private void update(Book book){
-        bookList.remove(book);
-        bookList.add(book);
+        public static void update(Book book){
+         delete(book.getID());
+         create(book);
     }
-    private Book getByBookId(int bookId){
-        for (int i = 0; i < bookList.size(); i++) {
-            if (bookList.get(i).getID() == bookId) {
-               return bookList.get(i);
+        public static Book getByBookId(int bookId){
+            for (int i = 0; i < bookList.size(); i++) {
+                if (bookList.get(i).getID() == bookId) {
+                   return bookList.get(i);
+                }
             }
-        }
-    return null;
+           return null;
     }
-    private List<Book> getAllBook(){
-        for (int i = 0; i < bookList.size(); i++) {
-            System.out.println(bookList.toString());
-        }
-        return null;
+        public static List<Book> getAllBook(){return bookList;    }
+        public static void getAllBookList(){
+            for (int i = 0; i < getAllBook().size(); i++) {
+                System.out.println( getAllBook().get(i).AllDatetoString());
+            }
     }
 }
+
 
